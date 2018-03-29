2be91f9038ad9369a9db8a33b2f977f234ca0496
Vijay Hebbar <vhhebbar@ncsu.edu>
2018-03-29 12:46:17 -0400

    Adding some source

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println(print("Vijay"));
+	}
+	
+	public static String print(String str) {
+		return "Hello " + str;
+	}
+	
+}
